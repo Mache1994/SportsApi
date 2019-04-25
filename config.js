3d313e9839d57982fa77d3c6159997a092aa0e17
Marcelo Martinez <marce_fmt@gmail.com>
2019-04-25 07:55:52 -0500

    Initial

---
+++ config.js
@@ -0,0 +1,5 @@
+
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb'
+exports.PORT = process.env.PORT || 8080;
+
+//'mongodb+srv://Marcelo:fernando@cluster0-pj8a4.mongodb.net/sports?retryWrites=true';
