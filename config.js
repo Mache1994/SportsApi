cd7685e6f150d416057f30f9b695b5eb0e569de0
Marcelo Martinez <marce_fmt@gmail.com>
2019-04-25 07:58:33 -0500

    config update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb'
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb:://localhost/sports'
 exports.PORT = process.env.PORT || 8080;
 
 //'mongodb+srv://Marcelo:fernando@cluster0-pj8a4.mongodb.net/sports?retryWrites=true';
